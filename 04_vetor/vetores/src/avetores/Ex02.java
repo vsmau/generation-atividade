69a5ca72f78f29fac6d91bffcf867962880e7d97
Vinicius <boringmkt@gmail.com>
2024-10-31 14:43:53 -0300

    Adicionar exercicio 2 de vetor

--- 04_vetor/vetores/src/avetores/Ex02.java
+++ 04_vetor/vetores/src/avetores/Ex02.java
@@ -1,10 +1,53 @@
+/*
+* Elabore um algoritmo que leia 10 números inteiros e 
+* armazene em um vetor. Em seguida, mostre na tela:
+* 	- Todos os elementos dos índices ímpares do vetor 
+* 	- Todos os elementos do vetor que são números pares
+* 	- A Soma de todos os elementos do vetor
+* 	- A Média de todos os elementos do vetor, armazenada
+* 	  em uma variável do tipo real
+ * */
+
 package avetores;
+
+import java.util.Scanner;
 
 public class Ex02 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Iniciar variáveis e o leitor
+		Scanner scanner = new Scanner(System.in);
+		int vetorLido[] = new int[10];
+		float soma = 0;
 
+		// Entrada dos números pelo teclado e soma
+		for (int i = 0; i < vetorLido.length; i++) {
+			System.out.println("Digite o " + (i + 1) + "º número de 10: ");
+			vetorLido[i] = scanner.nextInt();
+			soma += vetorLido[i];
+		}
+
+		// Saida dos números em índices impares
+		System.out.println("Elementos nos índices impares: ");
+		for (int i = 0; i < vetorLido.length; i++) {
+			if (i % 2 != 0) {
+				System.out.printf("%d ", vetorLido[i]);
+			}
+		}
+
+		// Saida dos Elementos pares
+		System.out.println("\nElementos pares: ");
+		for (int i = 0; i < vetorLido.length; i++) {
+			if (vetorLido[i] % 2 == 0) {
+				System.out.printf("%d ", vetorLido[i]);
+			}
+		}
+
+		// Saida da soma e média
+		System.out.printf("\nSoma: %.0f", soma);
+		System.out.println("\nMedia: " + soma / 10);
+
+		scanner.close();
 	}
 
 }
